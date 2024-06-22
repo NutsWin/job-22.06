1157f918ffcb9e2b58cd8982e3a28e1f06fba5ae
NutsWin <nutswinjob@yandex.ru>
2024-06-22 17:18:57 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,12 @@
+# Домашнее задание по теме "Организация программ и методы строк."
+
+my_string = input("Осваиваем новую тему по работе со строками? ")
+len(my_string)
+print(len(my_string))
+
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ' , ''))
+print(my_string[0])
+print(my_string[-1])
